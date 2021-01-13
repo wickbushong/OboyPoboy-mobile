624096845df2a5580b383b6b74a346f9fd09fe16
wickbushong <wickbushong@gmail.com>
2021-01-13 11:42:34 -0600

    Pad top for android.

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar } from 'react-native';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>blah de blah blah. give me a sandwich</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>blah de blah blah. give me a sandwich... please</Text>
+      <Button>Sup bih</Button>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -17,5 +19,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
   },
 });
